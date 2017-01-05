7acc13c85fdaa650cc5515a2ae4378b7736b046c
susanlovaglio <susan@flatironschool.com>
2017-01-04 22:43:35 -0500

    efficient image loading

---
+++ ComicCon/CharacterCell.swift
@@ -0,0 +1,13 @@
+//
+//  CharacterCell.swift
+//  ComicCon
+//
+//  Created by susan lovaglio on 12/22/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCell: UICollectionViewCell {
+    
+}
